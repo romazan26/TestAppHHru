f54a861d6b2c063c44b76feada8ac120f92923b9
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-03-13 14:31:30 +0300

    Initial Commit

---
+++ TestAppHHru/TestAppHHruApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppHHruApp.swift
+//  TestAppHHru
+//
+//  Created by Роман on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppHHruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
